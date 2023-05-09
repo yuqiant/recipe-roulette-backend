e0e3424c879178f80b282e6f4edd269e3466bc6f
yuqiant <122412689+yuqiant@users.noreply.github.com>
2023-05-09 11:02:40 -0400

    updated entry.py

--- src/entry.py
+++ src/entry.py
@@ -1,14 +1,19 @@
 import json
-
+from src.doa.db_manager import DBManager
+from src.inventory.inventory import Inventory
 
 def handler(event, context):
     # main entry point for lambda
     # return scrape.handler({"keyword": "egg"}, None)
-    return json.dumps({"message": "To be implemented"})
+    # return json.dumps({"message": "To be implemented"})
+    db_manager = DBManager()
+    inventory = Inventory(db_manager)
+    return inventory.handler(event, context)
 
 
 if __name__ == '__main__':
     print(handler({"key": "value"}, None))
+
 
 
 
