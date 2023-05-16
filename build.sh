6bee96c7cff79565ffad4eb3f3f9bebd50591899
Fengrui Gan <fengruigan@gmail.com>
2023-05-16 14:59:36 -0700

    add router.py

--- build.sh
+++ build.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-echo "Build starting"
+echo "Running Test"
+python -m unittest discover -s ./test || exit 1
 
+echo "Pre-build clean up cache"
+rm dist.zip
+find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete
+
+echo "Build starting"
 python -m pip install --upgrade pip
 pip install -r requirements.txt --target ./package
 cd package || exit 1
 zip -r ../dist.zip .
 cd ../src || exit 1
 zip -r ../dist.zip .
+
+echo "Post-build clean up"
+cd .. || exit 1
+rm -rf package
