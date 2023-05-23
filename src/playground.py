c7705a169ee47978f827518eed031af7c7f69d4e
yuqiant <122412689+yuqiant@users.noreply.github.com>
2023-05-23 18:00:27 -0400

    updated playground.py

--- src/playground.py
+++ src/playground.py
@@ -12,25 +12,27 @@ ingredient_route = IngredientRoute(db_manager)
 inventory_route = InventoryRoute(db_manager)
 
 # get an ingredient by user_id
-user_id = ObjectId("6463e5c66a39ed73a98958cc")
+user_id = "101010"
 user = user_route.handle_get_user(user_id)
 print(user)
 
 # get an ingredient by id
-ingredient_id = ObjectId("6463e5c66a39ed73a98958cc")
-ingredient = ingredient_route.handle_get_by_id(ingredient_id)
-print(ingredient)
+ingredient_id = ObjectId("646d2a4804e31f5e664e3441")
+ingredient = db_manager.get_collection("ingredients_json").find_one({"name": "pork"})
+ingredientObj = Ingredient.fromdict(ingredient)
+#ingredient = ingredient_route.handle_get_by_id(ingredient_id)
+print(ingredientObj)
 
 # get user inventory by user id
 inventory = inventory_route.handle_get_user_inventory(user_id)
 print(inventory)
 
 # add an ingredient to user inventory
-ingredient_id_to_add = ObjectId("6463e5c66a39ed73a98958cc")
+ingredient_id_to_add = ObjectId("646d2a4804e31f5e664e3441")
 result = inventory_route.handle_update_user_inventory(user_id, ingredient_id_to_add, "add")
 print(f"Add ingredient result: {result}")
 
 # remove an ingredient from user inventory
-ingredient_id_to_remove = ObjectId("6463e5c66a39ed73a98958cc")
+ingredient_id_to_remove = ObjectId("646d2a4804e31f5e664e3441")
 result = inventory_route.handle_update_user_inventory(user_id, ingredient_id_to_remove, "remove")
 print(f"Remove ingredient result: {result}")
